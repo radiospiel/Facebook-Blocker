dbb5778f021fc587aa6c94a14ad52b25e6fee1f6
Alex Rubin <alex@webgraph.com>
2011-03-23 18:45:57 -0400

    Chrome 1.2.3 update to fix blocking issue on Facebook.com

--- chrome/source/facebookBlocker/fbblock.js
+++ chrome/source/facebookBlocker/fbblock.js
@@ -6,24 +6,29 @@ document.addEventListener('load', handleBeforeLoadEvent, true);
 function handleBeforeLoadEvent(event) {
 	const element = event.target;
 
-	if(element.nodeName == 'IFRAME'){
-		if(element.src.toLowerCase().indexOf('facebook.com/extern/') > 0){
-			element.parentNode.removeChild(element);
-		}else if(element.src.toLowerCase().indexOf('facebook.com/plugins/') > 0){
-			element.parentNode.removeChild(element);
-		}else if(element.src.toLowerCase().indexOf('fbshare.me') > 0){
-			element.parentNode.removeChild(element);
-		}
-	}else if(element.nodeName == 'SCRIPT'){
-		if(element.src.toLowerCase().indexOf('static.ak.fbcdn.net') > 0){
-			event.preventDefault();
-			return false;
-		}else if(element.src.toLowerCase().indexOf('static.ak.connect.facebook.com') > 0){
-			return false;
-		}else if(element.src.toLowerCase().indexOf('fbshare.me') > 0){
-			return false;
-		}else if(element.src.toLowerCase().indexOf('api.facebook.com') > 0){
-			return false;
+	if(pageURL != 'www.facebook.com'){
+		if(element.nodeName == 'IFRAME'){
+			if(element.src.toLowerCase().indexOf('facebook.com/extern/') > 0){
+				element.parentNode.removeChild(element);
+			}else if(element.src.toLowerCase().indexOf('facebook.com/plugins/') > 0){
+				element.parentNode.removeChild(element);
+			}else if(element.src.toLowerCase().indexOf('fbshare.me') > 0){
+				element.parentNode.removeChild(element);
+			}
+		}else if(element.nodeName == 'SCRIPT'){
+			if(element.src.toLowerCase().indexOf('static.ak.fbcdn.net') > 0){
+				event.preventDefault();
+				return false;
+			}else if(element.src.toLowerCase().indexOf('static.ak.connect.facebook.com') > 0){
+				return false;
+			}else if(element.src.toLowerCase().indexOf('fbshare.me') > 0){
+				return false;
+			}else if(element.src.toLowerCase().indexOf('api.facebook.com') > 0){
+				return false;
+			}else{
+				return true;
+				event.preventDefault();
+			}
 		}else{
 			return true;
 			event.preventDefault();
