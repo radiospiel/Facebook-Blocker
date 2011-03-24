9f9de9c594b302ad27aba5b6d716dff78403f607
Alex Rubin <alex@webgraph.com>
2011-03-24 12:18:23 -0400

    Opera user.js script for Opera 10 and earlier

---
+++ opera/facebookblocker user js file/fbblock.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name Facebook Blocker
+// @author J. King & Dustin Wilson
+// @version 1.
+// @description Removes annoying Facebook garbage from websites.
+// @exclude http://*.facebook.*/*
+// ==/UserScript==
+
+window.opera.addEventListener('BeforeScript',function(ev)
+{
+ if(ev.element.src.match(/^https?:\/\/(?:[^\/]+\.)*(?:facebook|fbcdn|fbshare)\.[^\/]+\//))
+  ev.preventDefault();
+},false);
+
+//There will be a flash of content sometimes before loading. This is unavoidable because Opera starts loading before the DOM tree is finished construction in an attempt to perhaps get relevant information to the user more quickly.
+window.opera.addEventListener('BeforeEvent.load',function(ev)
+{
+ 	var element=ev.event.target;
+
+ if(element.nodeName=='IFRAME' && element.src.match(/^https?:\/\/(?:[^\/]+\.)*(?:facebook|fbcdn|fbshare)\.[^\/]+\//))
+  element.parentNode.removeChild(element);
+},false);
