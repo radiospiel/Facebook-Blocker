9e89c2ec363cea1c18385ab718d1388734b53216
Alex Rubin <alex@webgraph.com>
2011-06-30 22:02:47 -0400

    opera updates

--- opera/source/includes/fbblock.js
+++ opera/source/includes/fbblock.js
@@ -7,17 +7,18 @@
 // @exclude https://*.facebook.*/*
 // ==/UserScript==
 
-window.opera.addEventListener('BeforeScript',function(ev)
-{
- if(ev.element.src.match(/^https?:\/\/(?:[^\/]+\.)*(?:facebook|fbcdn|fbshare|fb|akamaihd)\.[^\/]+\//))
-  ev.preventDefault();
+window.opera.addEventListener('BeforeScript',function(ev) {
+	if(ev.element.src.match(/^https?:\/\/(?:[^\/]+\.)*(?:facebook|fbcdn|fbshare|fb|akamaihd)\.[^\/]+\//)){
+		ev.preventDefault();
+	}else{
+		alert(ev.element.src);
+	}
 },false);
 
 //There will be a flash of content sometimes before loading. This is unavoidable because Opera starts loading before the DOM tree is finished construction in an attempt to perhaps get relevant information to the user more quickly.
-window.opera.addEventListener('BeforeEvent.load',function(ev)
-{
+window.opera.addEventListener('BeforeEvent.load',function(ev) {
  	var element=ev.event.target;
 
- if(element.nodeName=='IFRAME' && element.src.match(/^https?:\/\/(?:[^\/]+\.)*(?:facebook|fbcdn|fbshare|fb|akamaihd)\.[^\/]+\//))
-  element.parentNode.removeChild(element);
+	if(element.nodeName=='IFRAME' && element.src.match(/^https?:\/\/(?:[^\/]+\.)*(?:facebook|fbcdn|fbshare|fb|akamaihd)\.[^\/]+\//))
+		element.parentNode.removeChild(element);
 },false);
